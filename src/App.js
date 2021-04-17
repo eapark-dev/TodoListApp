699672905275dc14804f7353e6338cbbae493148
eapark-dev <eapark5908@gmail.com>
2021-04-17 16:24:11 +0900

    create todolist

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* 메인화면 */}
+      <div id="wrap_head">
+       
+      </div>
+    </div>
+  );
+}
+
+export default App;
